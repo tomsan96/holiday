13311a7b25e30ba9c5b5303f42cd54a56fb9ba14
tomsan96 <gradada0507@gmail.com>
2024-04-27 15:38:17 +0900

    first commit

---
+++ holiday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  holiday
+//
+//  Created by 山崎定知 on 2024/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ holiday/holidayApp.swift
@@ -0,0 +1,17 @@
+//
+//  holidayApp.swift
+//  holiday
+//
+//  Created by 山崎定知 on 2024/04/27.
+//
+
+import SwiftUI
+
+@main
+struct holidayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
